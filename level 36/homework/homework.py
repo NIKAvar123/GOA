3b3081e93f111991983726389f645a6f7bdb263a
NIKAvar123 <bastibubu31@gmail.com>
2024-09-15 12:00:38 +0400

    first commit

---
+++ level 36/homework/homework.py
@@ -0,0 +1,35 @@
+#def in_asc_order(arr):
+   #for i in range(1,len(arr)):
+       # if arr[i - 1] > arr[i]:
+        #    return False
+    
+   # return True
+
+
+
+
+
+
+
+#def repeats(arr):
+#    sum = 0
+    
+ #   for num in arr:
+  #      if arr.count(num) == 1:
+   #         sum += num
+    
+   # return sum
+
+
+
+#def capitalize(s):
+ #   even_caps = ""
+  #  odd_caps = ""
+   # for i in range(len(s)):
+    #       even_caps += s[i].upper()
+     #       odd_caps += s[i]
+      #  else:
+       #     even_caps += s[i]
+        #    odd_caps += s[i].upper()
+    
+   # return [even_caps,odd_caps]
