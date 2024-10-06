31dbbf563c2d964f05a4bd72d208c780f614e0e1
NIKAvar123 <bastibubu31@gmail.com>
2024-10-06 13:47:10 +0400

    first commit

---
+++ level 41/classwork/class.js
@@ -0,0 +1,35 @@
+const nika={
+    age:13,
+    favourite_car:"bmw",
+    pet:false,
+    fav_sport:"football"
+    
+}
+const dato={
+    age:14,
+    favourite_car:"mercedes",
+    pet:false,
+    fav_sport:"rugby"
+}
+const mate={
+    age:13,
+    favourite_car:"porshe",
+    pet:true,
+    fav_sport:"football"
+}
+//ობიექტი არის ის რასაც ვხედავტ ვეხებით და ასევე მას აქვს ფუნქციები
+
+
+const niks={
+    age:13,
+    favourite_car:"bmw",
+    pet:false,
+    fav_sport:"football"
+}
+delete niks.age
+
+niks.weight=58
+
+niks.age=14
+
+
---
+++ level 41/classwork/main.js
@@ -0,0 +1,7 @@
+const p=document.getElementById("nn");
+p.style.color="blue";
+p.style.backgroundColor="yellow"
+
+const kk=document.getElementById("kk");
+kk.style.color="green";
+kk.style.backgroundColor="yellow";
