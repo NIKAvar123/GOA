f32a34f881b4856086149ff15c1b6848e6722749
NIKAvar123 <bastibubu31@gmail.com>
2024-11-13 17:45:10 +0400

    first commit

--- level 52/homework/home.py
+++ level 52/homework/home.py
@@ -1,27 +1,25 @@
 import math
 
 def count_area(*args):
+
     if len(args) == 2:
         length, width = args
         return length * width
-    
+
+
     elif len(args) == 1:
-       
         radius = args[0]
-        return math.pi * radius ** 2
-    
+        return math.pi * (radius ** 2)
+
+
     elif len(args) == 3:
-        
-        a, b, c = args
-        
-        s = (a + b + c) / 2  
-        area = math.sqrt(s * (s - a) * (s - b) * (s - c))
-        return area
-    
+        base, height, _ = args
+        return (base * height) / 2
+
     else:
-        return "შეცდომა: ფიგურა არ ითქვა ან გადაცემულია არასწორი რაოდენობის პარამეტრები"
+        return "გთხოვთ, გადმოეცით სწორების რაოდენობის არგუმენტები"
 
 
-print(count_area(5, 3))   
-print(count_area(7))     
-print(count_area(3, 4, 5)) 
+print(count_area(5, 10))  
+print(count_area(7))    
+print(count_area(6, 8, 0)) 
