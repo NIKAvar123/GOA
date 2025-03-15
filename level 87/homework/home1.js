15b1b816244d911f32376a64e9b39029430ad27c
NIKAvar123 <bastibubu31@gmail.com>
2025-03-15 17:29:33 +0400

    first commit

---
+++ level 87/homework/home1.js
@@ -0,0 +1,4 @@
+let text = "goa";
+let result = text.repeat(5);
+
+console.log(result)
---
+++ level 87/homework/home3.js
@@ -0,0 +1,4 @@
+let classDivs = document.querySelectorAll(".classDiv");
+classDivs.forEach((div) => {
+    div.style.borderRadius = "10px";
+});
