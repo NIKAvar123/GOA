e22a1b2d1ec7e31abcd7841778ee5e543e1d5d7d
NIKAvar123 <bastibubu31@gmail.com>
2025-03-22 19:08:23 +0400

    first commit

---
+++ level 89/homework/home.py
@@ -0,0 +1,5 @@
+text = input("sheiyvanet texti: ")
+start = int(input("sheiyvanet dasawyisi indeqsi: "))
+
+sliced_text = text[start:] 
+print("shedegi:", sliced_text)
---
+++ level 89/homework/home1.js
@@ -0,0 +1,5 @@
+let text = prompt("sheiyvanet texti:");
+let start = parseInt(prompt("sheiyvanet dasawyisi indexi:"));
+
+let slicedText = text.slice(start);  
+console.log("shedegi:", slicedText);
