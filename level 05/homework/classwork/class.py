95a0f8de18a53b74a542df36cebd4c19313980a4
NIKAvar123 <bastibubu31@gmail.com>
2025-05-07 22:17:26 +0400

    first commit

--- level 05/homework/classwork/class.py
+++
@@ -1,7 +0,0 @@
-fav_cars = []
-
-for i in range(5):
-    car = input("Please enter your fav car: ")
-    fav_cars.append(car)
-
-print(fav_cars)
--- level 05/homework/day18/day18.py
+++
@@ -1,8 +0,0 @@
-person={
-    "name":"nika",
-    "age":13
-
-    
-}
-update=({"gmail":"bastibubu31@gmail.com"})
-update=({"surname"="mumladze"})
